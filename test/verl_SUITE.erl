9c628f7ec2f0817473e26b8f56b59e6cd3e7c222
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2021-05-19 21:50:27 +0100

    Up the CI game
    * bump rebar3_lint
    * introduce rebar3_hank

--- test/verl_SUITE.erl
+++ test/verl_SUITE.erl
@@ -1,6 +1,7 @@
 -module(verl_SUITE).
 
 -compile(export_all).
+-hank([{unnecessary_function_arguments, all}]).
 
 -include_lib("common_test/include/ct.hrl").
 -include_lib("stdlib/include/assert.hrl").
